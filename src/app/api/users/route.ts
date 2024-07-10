cbc4102d4e423c412a5742dcdc796700d0a0c09c
ValentinZoia <valentinzoia@gmail.com>
2024-07-10 00:37:10 -0300

    agregando endpoint user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,3 +1,21 @@
-export function GET(){
-    
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/User";
+import { messages } from "@/utils/messages";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest){
+    try {
+        await connectDB();
+        const users = await User.find();
+        return NextResponse.json({users}, {status: 200});
+
+
+
+
+    } catch (error) {
+        return NextResponse.json(
+            { error: messages.error.generic },
+            { status: 500 }
+          );
+    }
 }
